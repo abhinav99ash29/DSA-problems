70b0fb2f6efd66f8545afc6a6e9091ee75eb0351
Abhinav Gupta <abhinav99ash29@gmail.com>
2021-10-08 23:27:29 +0530

    Update squared-of-a-sorted-array
    https://leetcode.com/problems/squares-of-a-sorted-array/

--- leetcode/squares-of-a-sorted-array.cpp
+++ leetcode/squares-of-a-sorted-array.cpp
@@ -1,11 +1,31 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
+        int n = nums.size(); int fp=n-1;
+        vector<int> ans;
         for(int i=0;i<n;i++) {
-            nums[i] = pow(nums[i], 2);
+            if(nums[i]>=0)
+            {fp=i; break;}
+            
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        int i=fp; fp--;
+        while(i<n && fp>=0) {
+              if(abs(nums[i]) < abs(nums[fp])) {
+                    ans.push_back(nums[i]*nums[i]);
+                  i++;
+                } else {
+                  ans.push_back(nums[fp]*nums[fp]);
+                  fp--;
+              }
+        }
+        while(fp>=0){
+            ans.push_back(nums[fp]*nums[fp]);
+            fp--;
+        }
+        while(i<n) {
+         ans.push_back(nums[i]*nums[i]);
+        i++;
+        }
+        return ans;
     }
 };
