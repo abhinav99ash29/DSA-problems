0a0e73582100c6f1faf2eed9b7267225aff13b32
Abhinav Gupta <abhinav99ash29@gmail.com>
2022-03-11 00:52:11 +0530

    Create reverse-linked-list-recursive.cpp

---
+++ leetcode/reverse-linked-list-recursive.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head, ListNode* prev=NULL) {
+        if (head == NULL) return prev;
+        ListNode* temp = head->next;
+        head->next = prev;
+        return reverseList(temp, head);
+    }
+};
