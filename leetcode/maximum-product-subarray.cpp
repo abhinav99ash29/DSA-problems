53d7bda8607ef04d1d8fc43a276c18eec3771e4b
Abhinav Gupta <abhinav99ash29@gmail.com>
2022-03-21 23:26:56 +0530

    Create maximum-product-subarray.cpp

---
+++ leetcode/maximum-product-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        if (n==0) return 0;
+        int ans = INT_MIN, l=0, r=0;
+        for(int i=0;i<n;i++) {
+            l = (l ? l : 1)*nums[i];
+            r = (r ? r : 1)*nums[n-1-i];
+            ans = max(ans, max(l,r));
+        }
+        return ans;
+    }
+};
