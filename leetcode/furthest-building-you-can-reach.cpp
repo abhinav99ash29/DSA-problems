9617b31cca61754c6f16a551216c502d622ef475
Abhinav Gupta <abhinav99ash29@gmail.com>
2022-06-22 22:31:26 +0530

    Create furthest-building-you-can-reach.cpp

---
+++ leetcode/furthest-building-you-can-reach.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int> q;
+        for(int i=0;i<heights.size()-1;i++) {
+            int d = heights[i+1] - heights[i];
+            if (d>0)
+                q.push(-d);
+            if (q.size()>ladders) {
+                bricks+=q.top();
+                q.pop();
+            }
+            if (bricks<0) return i;
+        }
+        return heights.size()-1;
+    }
+};
