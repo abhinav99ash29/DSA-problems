00aa22ce4b59f268dd45f8488f35a01a58078edb
Abhinav Gupta <abhinav99ash29@gmail.com>
2022-09-11 22:11:47 +0530

    Create set-matrix-zeroes.cpp

---
+++ leetcode/set-matrix-zeroes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int r = matrix.size();
+        int c = matrix[0].size();
+        set<int> row, col;
+        for(int i=0;i<r;i++) {
+            for(int j=0;j<c;j++) {
+                if (matrix[i][j]==0) {
+                    row.insert(i);
+                    col.insert(j);
+                }
+            }
+        }
+        for(auto& i: row) {
+            for(int j=0;j<c;j++) {
+                matrix[i][j] = 0;
+            }
+        }
+        for(auto& j: col) {
+            for(int i=0;i<r;i++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+};
