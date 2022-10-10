b125c426a245a57ca34fa9eb4ee4958e08368c88
Abhinav Gupta <abhinav99ash29@gmail.com>
2022-10-10 17:45:56 +0530

    Create break-a-palindrome-better.cpp

---
+++ leetcode/break-a-palindrome-better.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string breakPalindrome(string pal) {
+        int n = pal.size();
+        if (n<=1) return "";
+        for(char ch='a';ch<='b';ch++) {
+            int i=0, j=n-1;
+            while(i<j) {
+                if (pal[i]!=ch) {
+                    if (pal[i]<ch) {
+                        pal[j] = ch;
+                        return pal;
+                    } else {
+                        pal[i] = ch;
+                        return pal;
+                    }
+                }
+                i++;
+                j--;
+            }
+            
+        }
+        return "";
+    }
+};
