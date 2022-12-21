f7ef94815784a8731d077411ebb07d1bf22a1d95
Abhinav Gupta <abhinav99ash29@gmail.com>
2022-12-21 23:16:29 +0530

    Create keys-and-rooms.cpp

---
+++ leetcode/keys-and-rooms.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    void dfs(int i, vector<vector<int>>& rooms, vector<bool>& vis) {
+        vis[i] = true;
+        for(int& v: rooms[i]) {
+            if (!vis[v]) {
+                dfs(v, rooms, vis);
+            }
+        }
+    }
+
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<bool> vis(n, false);
+        dfs(0, rooms, vis);
+        for(int i=0;i<n;i++) {
+            if (!vis[i]) return false;
+        }
+        return true;
+    }
+};
