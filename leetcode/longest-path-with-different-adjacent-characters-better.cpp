450f1f3bb3f5056bba12bddc1786031cd8011d69
Abhinav Gupta <abhinav99ash29@gmail.com>
2023-01-14 02:37:29 +0530

    Create longest-path-with-different-adjacent-characters-better.cpp

---
+++ leetcode/longest-path-with-different-adjacent-characters-better.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    int fn(int v, vector<vector<int>>& child, string& s, int& mx) {
+        int l = 0, r = 0;
+        for(int i=0;i<child[v].size();i++) {
+            int x = child[v][i];
+            int f = fn(x,child,s,mx);
+            if (s[v] == s[x]) continue;
+            if (r<f) {
+                r = f;
+                if (r>l) {
+                    swap(r, l);
+                }
+            }
+        }
+        mx = max(mx, 1+l+r);
+        return 1+l;
+    }
+
+
+    int longestPath(vector<int>& par, string s) {
+        int mx = 1;
+        int n = par.size();
+        vector<vector<int>> child(n);
+        for(int i=1;i<n;i++) {
+            child[par[i]].push_back(i);
+        }
+        fn(0,child, s, mx);
+        return mx;
+    }
+};
+
